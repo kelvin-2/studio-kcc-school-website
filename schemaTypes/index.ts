53efb440ddcb80c3d217abba637779559a26ffc1
Kelvin <kelvinyounglee47@gmail.com>
2025-08-23 02:23:00 +0200

    finshed using sanity for homePage

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,8 +1,11 @@
 import heroSection from './heroSection'
 import boardingTransport from './boardTransportSection'
 import progamImages from './programImages'
+import teachersSection from './teachersSection'
 
 export const schemaTypes = [heroSection,
     boardingTransport,
-    progamImages
+    progamImages,
+    teachersSection
+
 ]
