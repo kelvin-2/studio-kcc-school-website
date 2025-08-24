ab4dc4954dbc455019971f20d0dd1d34cca840f4
Kelvin <kelvinyounglee47@gmail.com>
2025-08-24 14:57:11 +0200

    added scheme for about'

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,10 +2,12 @@ import heroSection from './heroSection'
 import boardingTransport from './boardTransportSection'
 import progamImages from './programImages'
 import teachersSection from './teachersSection'
+import missionVision from './missionVision'
 
 export const schemaTypes = [heroSection,
     boardingTransport,
     progamImages,
-    teachersSection
+    teachersSection, 
+    missionVision
 
 ]
